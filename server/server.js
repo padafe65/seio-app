52be2b5526c6a79e3e9846ba95923de4053bb317
padafe65 <padafe654@gmail.com>
2025-04-08 18:37:17 -0500

    Actualización: cambios en server.js password

--- server/server.js
+++ server/server.js
@@ -24,7 +24,8 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD || '',
+
     database: process.env.DB_NAME
 });
 
