563f229d82385293617dedb5bd1aeb4ccecb1e0a
padafe65 <35849057+padafe65@users.noreply.github.com>
2025-04-08 20:54:46 -0500

    Update AuthContext.js

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -2,8 +2,8 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 const AuthContext = createContext();
-const API_URL = process.env.REACT_APP_API_URL;
 
 const login = async (datos) => {
   try {
